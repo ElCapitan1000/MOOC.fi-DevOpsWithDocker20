0aab5a9dd1e65a373c9c99ff40beafb099e7bbbe
El Captian <ElCapitan@PC192.168.2.106.speedport.ip>
2021-01-10 13:09:31 +0100

    Exercise3.3 works!

--- part3/Exercise3.3/script.sh
+++ part3/Exercise3.3/script.sh
@@ -1,41 +1,35 @@
 #!/bin/bash
 
-echo "I am the script"
-git --version
-docker --version
+echo git --version
+echo docker --version
 
-# clone the app to build
-#git clone https://github.com/docker-hy/frontend-example-docker
-# clone the Dockerfile to build the app
-#git clone https://github.com/ElCapitan1000/app-for-exercise3.3
+echo "-- clone the app to build and the Dockerfile to build the app --"
+git clone 'https://github.com/ElCapitan1000/app-for-exercise3.3'
 
-ls -l
-
-
-if [ -z $1 ]  || [ -z $2 ] || [ -z $3 ] || [ -z $4 ]; then
-    echo "USAGE: sudo docker run -it -v /var/run/docker.sock:/var/run/docker.sock app-exercise3.2 
-	<repo_url> <repo docker name> <dockerid> <dockerpassword>"
+if [ -z $1 ]  || [ -z $2 ] || [ -z $3 ]; then
+    echo "!!! You must provide the arguments: <dockerid> <dockerpassword> <appname>
+	 to push to docker registry. Use whatever <appname> you want"	  
     exit
 fi
 
-app_name=app-for-exercise3.3
+docker_id=$1
+docker_password=$2
+app_name=$3
 
-#url=$1
-#docker_name=$2
-docker_id=elcapitan1000
-#docker_password=$4
-
-
-# build, tag and push image
-docker build -t $app_name .
+echo "-- build, tag and push image --"
+docker build -t $app_name 'app-for-exercise3.3/.'
 docker tag $app_name $docker_id/$app_name
-#docker push $docker_id/$app_name
 
-docker run -d --rm -p 5000:5000 --name app-33 $app_name
-#docker ps
+echo "-- login to docker --"
+echo "$docker_password" | docker login -u $docker_id --password-stdin
 
-echo "$@"
-echo "$0: $1 $2 $3 $4"
-echo "$# arguments"
-echo "$app_name"
+
+echo "-- start to push to $docker_id/$app_name --"
+docker push $docker_id/$app_name
+
+echo "Run the container with:
+	sudo docker run -it -p 5000:5000 $3"
+
+
+
 
